8ca515611dc173c33c0ba88b587db1b1b1adfacb
Lucas Souza <lucas.souzaduarte.73@gmail.com>
2023-08-28 16:44:47 -0300

    finalizando

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify')
+const uglify = require('gulp-uglify');
 
 function scripts() {
     return gulp.src('./src/scripts/*.js')
